d888ded95370088a6285429405b70f38570fe769
tuanmjnh <minhtuan200990@gmail.com>
2019-07-11 17:23:28 +0700

    tm

--- src/config/oracle.js
+++ src/config/oracle.js
@@ -26,7 +26,7 @@ module.exports = {
     poolMin: 1, // let the pool shrink completely
     poolMax: 10, // maximum size of the pool
     poolIncrement: 1, // only grow the pool by one connection at a time
-    // poolTimeout: 0, // never terminate idle connections
+    poolTimeout: 0, // never terminate idle connections
     externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
   },
   // Knex
